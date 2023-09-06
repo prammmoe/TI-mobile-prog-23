11d3b7008d1daa729a7d648786323c564839bee2
flatmoon101 <rubens.juristanto4@gmail.com>
2023-09-06 10:43:24 +0700

    tambah font, xml select, regis, login

--- app/src/main/java/com/example/metric/Selection.kt
+++ app/src/main/java/com/example/metric/Selection.kt
@@ -1,11 +1,23 @@
 package com.example.metric
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
 class Selection : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.selection)
+        val registerButton: Button = findViewById(R.id.button_register)
+        registerButton.setOnClickListener {
+            val intent = Intent(this@Selection, register::class.java)
+            startActivity(intent)
+        }
+
+        val loginButton: Button = findViewById(R.id.button_login)
+        loginButton.setOnClickListener {
+            val intent = Intent(this@Selection, login::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/metric/register.kt
+++ app/src/main/java/com/example/metric/register.kt
@@ -1,4 +1,12 @@
 package com.example.metric
 
-class register {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+class register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register)
+    }
 }
