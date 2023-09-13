1ad2e733074f16b9f5789acc1430ad718af843f5
flatmoon101 <rubens.juristanto4@gmail.com>
2023-09-13 11:40:49 +0700

    adapt semua onboard, ubah button ke text with event

--- app/src/main/java/com/example/metric/Login.kt
+++ app/src/main/java/com/example/metric/Login.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class Login: AppCompatActivity() {
@@ -11,7 +12,7 @@ class Login: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
-        val registerText: Button = findViewById(R.id.buttonRegister)
+        val registerText: TextView = findViewById(R.id.buttonRegister)
         registerText.setOnClickListener {
             val intent = Intent(this@Login, Register::class.java)
             startActivity(intent)
--- app/src/main/java/com/example/metric/Register.kt
+++ app/src/main/java/com/example/metric/Register.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
@@ -18,7 +19,7 @@ class Register : AppCompatActivity() {
 
         auth = Firebase.auth
 
-        val loginText: Button = findViewById(R.id.buttonLogin)
+        val loginText: TextView = findViewById(R.id.buttonLogin)
         loginText.setOnClickListener {
             val intent = Intent(this@Register, Login::class.java)
             startActivity(intent)
