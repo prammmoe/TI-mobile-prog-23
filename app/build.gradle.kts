8989472e5d1f3b5ae19e08afc01a9a2e9b11154a
prammmoe <ikhwanpramuditha05@gmail.com>
2023-09-15 15:32:08 +0700

    fix: gradle build to emu still error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.metric"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.metric"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -44,10 +47,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    // Import the BoM for the Firebase platform
     implementation(platform("com.google.firebase:firebase-bom:32.2.3"))
-
-    // Add the dependency for the Firebase Authentication library
-    // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-auth-ktx")
 }
--- app/src/main/java/com/example/metric/Login.kt
+++ app/src/main/java/com/example/metric/Login.kt
@@ -2,27 +2,50 @@ package com.example.metric
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.metric.databinding.LoginBinding
+import com.example.metric.databinding.RegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import java.util.zip.Inflater
 
 class Login: AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var binding: LoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
+        binding = LoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+//        Ini kalo udah regis langsung klik text login aja
         val registerText: TextView = findViewById(R.id.buttonRegister)
         registerText.setOnClickListener {
             val intent = Intent(this@Login, Register::class.java)
             startActivity(intent)
         }
 
-        val email = findViewById<EditText>(R.id.emailLog)
-        val textEmail = email.text
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.loginLogin.setOnClickListener {
+            val email = binding.emailLog.text.toString()
+            val pass = binding.passLog.text.toString()
 
-        val pass = findViewById<EditText>(R.id.passLog)
-        val textPassword = pass.text
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this@Login, MainPage::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please fill all fields.", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         // ini Test
         val loginButton: Button = findViewById(R.id.login_login)
