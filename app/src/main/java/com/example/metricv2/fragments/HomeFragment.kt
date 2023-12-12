41162bf7f0b081f1dbdc13a05ce48fa25ea6db05
prammmoe <ikhwanpramuditha05@gmail.com>
2023-12-12 15:10:37 +0700

    feat(home): finished build UI, add border radius to trending section, and fix btm nav color logic

--- app/src/main/java/com/example/metricv2/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/metricv2/fragments/HomeFragment.kt
@@ -1,18 +1,19 @@
 package com.example.metricv2.fragments
 
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintSet.Motion
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
-import com.example.metricv2.R
 import com.example.metricv2.databinding.FragmentHomeBinding
 import com.example.metricv2.viewmodel.HomeViewModel
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeFragment : Fragment() {
 
@@ -38,6 +39,21 @@ class HomeFragment : Fragment() {
         homeMVVM.getRandomFood()
         observeRandomFood()
 
+    }
+    fun buttonEffect(button: View) {
+        button.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    v.background.setColorFilter(-0x1f0b8adf, PorterDuff.Mode.SRC_ATOP)
+                    v.invalidate()
+                }
+                MotionEvent.ACTION_UP -> {
+                    v.background.clearColorFilter()
+                    v.invalidate()
+                }
+            }
+            false
+        }
     }
 
     private fun observeRandomFood() {
